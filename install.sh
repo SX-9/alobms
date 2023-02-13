39d7d579fbc5eba8d5c96171267009317f43108c
sx9dev <sx-91@outlook.com>
2023-02-13 20:12:40 +0700

    h

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
-# Arch Linux Openbox Simple Setup
-```bash
-# Install Required Packages
+#! /bin/bash
 
 pacman -S openbox picom dunst tint2 conky rofi \
   pulseaudio nitrogen nodejs npm \
@@ -8,12 +6,7 @@ pacman -S openbox picom dunst tint2 conky rofi \
   lxappearance
 yay -S pa-applet-git obmenu-generator
 
-# Copy Config Files
-
 cd ~
 git clone https://github.com/SX-9/alobms .
 
-# Start Openbox
-
 startx
-```
